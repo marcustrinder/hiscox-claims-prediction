f7eb7b261a060591b4a27fcb8395786f2f91d2b5
marcustrinder <52547132+marcustrinder@users.noreply.github.com>
2024-06-12 13:57:00 +0100

    Update model_evaluation.py

--- scripts/model_evaluation.py
+++ scripts/model_evaluation.py
@@ -1,6 +1,7 @@
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score, confusion_matrix, log_loss, roc_curve
 import matplotlib.pyplot as plt
 import joblib
+# test PR
 
 def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
